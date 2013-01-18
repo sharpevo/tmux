ea8303b0e6b6f1c9c0e47cd2501909cdf2ea3317
sharpevo <sharpevo@gmail.com>
2013-01-18 14:57:44 +0800

    DOC is useless than MAIN

--- tmux_dev.sh
+++ tmux_dev.sh
@@ -11,13 +11,10 @@ tmux has-session -t $session_name
 
 if [ $? != 0 ]
 then
-    tmux new-session -s $session_name -n EDITOR -d
+    tmux new-session -s $session_name -n MAIN -d
     tmux split-window -h -t $session_name
     tmux split-window -v -p 30 -t $session_name:1.2
     tmux send-keys -t $session_name:1.2 'gl -n 7' C-m
-
-    tmux new-window -n DOC -t $session_name
-    tmux split-window -h -t $session_name
 
     tmux select-window -t $session_name:1
     tmux select-pane -t $session_name:1.1
